5927ad2e234f0b2b7121bda87379b828dc4ae9cd
Drunkcat7 <Miaopasc@gmail.com>
2023-03-29 12:02:11 +0800

    login页更新界面框架

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,25 @@
 <template>
+    <main>
+        <div class="title">
+            <span>欢迎使用</span>
+            <h1>广职选舍平台</h1>
+        </div>
 
+        <div class="login_from">
+            <div class="username">
+                <input id="Username" type="text" placeholder="学生考生号">
+                <span>请输入学生考生号</span> 
+            </div>
+            <div class="password">
+                <input id="Password" type="password" placeholder="学生身份证">
+                <span>请输入学生身份证</span>
+            </div> 
+            <div class="submit">
+                <div id="Submit">登录</div>
+            </div>   
+        </div>
+
+    </main>
 </template>
 
 <script setup>
