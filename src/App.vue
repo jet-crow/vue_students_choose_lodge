842231c3e66c12a5a6d954e868b1e51d8bb42ca4
Jet <1298874688@qq.com>
2023-03-29 08:56:37 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <div>
+      <el-button>你好呀</el-button>
+      <van-button type="primary">主要按钮</van-button>
+    </div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const {defineConfig} = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const {VantResolver} = require('unplugin-vue-components/resolvers');
+const {ElementPlusResolver} = require("unplugin-vue-components/resolvers");
+
+module.exports = defineConfig({
+    //基本路径
+    //默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
+    publicPath: './',
+
+    //输出文件目录
+    outputDir: 'dist',
+    indexPath: 'index.html',
+    //css相关配置
+    css: {
+        //是否使用css分离插件ExtractTextPlugin
+        extract: true,
+        //开启CSS sourcemaps?
+        sourceMap: false,
+    },
+    //生产环境是否生成sourceMap文件
+    productionSourceMap: false,
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver(), VantResolver()],
+            }),
+        ],
+    },
+});
