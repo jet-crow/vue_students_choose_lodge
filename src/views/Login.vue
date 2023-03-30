9a8ae1869f7f5fb2d501693bf9166aca040af8b2
Jet <1298874688@qq.com>
2023-03-30 10:53:25 +0800

    login 页面更新

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,15 +8,16 @@
 
             <div class="login_from">
                 <div class="username">
-                    <input id="Username" type="text" placeholder="学生考生号">
-                    <span>请输入学生考生号</span>
+                    <input id="Username" type="text" @blur="checkStudentInfo" v-model.number="candidateNumber"
+                        placeholder="学生考生号">
+                    <span v-show="showCNumber">请输入学生考生号</span>
                 </div>
                 <div class="password">
-                    <input id="Password" type="password" placeholder="学生身份证">
-                    <span>请输入学生身份证</span>
+                    <input id="Password" type="password" @blur="checkStudentInfo" v-model="idCard" placeholder="学生身份证">
+                    <span v-show="showIdCard">请输入学生身份证</span>
                 </div>
                 <div class="submit">
-                    <div id="Submit">登录</div>
+                    <div id="Submit" @click="login">登录</div>
                 </div>
             </div>
         </main>
@@ -24,6 +25,22 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const candidateNumber = ref('');
+const idCard = ref('');
+const showCNumber = ref(false), showIdCard = ref(false);
+
+//验证
+function checkStudentInfo() {
+    candidateNumber.value == '' ? showCNumber.value = true : showCNumber.value = false;
+    idCard.value == '' ? showIdCard.value = true : showIdCard.value = false;
+    return !(showCNumber.value || showIdCard.value);
+}
+
+function login() {
+    console.log(checkStudentInfo());
+}
 
 </script>
 
