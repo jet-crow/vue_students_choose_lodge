11b96f56ae4567534f35ffd2687214550b87966c
Drunkcat7 <Miaopasc@gmail.com>
2023-04-14 14:51:15 +0800

    update buildingmanagement

--- src/views/admin/BuildingManagement.vue
+++ src/views/admin/BuildingManagement.vue
@@ -1,30 +1,70 @@
 <!-- 宿楼管理 -->
 <template>
-      <el-button type="primary" class="">新建宿舍楼</el-button>
-      <main>
-        <section class="new_building_btn">
-            +
+    <main>
+        <section :onClick="() => { showFloorAdd = true }" class="new_building_btn">
+            新建 宿舍楼
         </section>
-        <div class="building_box"></div>
-      </main>
+
+
+        <el-dialog v-model="showFloorAdd" title="新建宿舍楼" width="30%" :before-close="handleClose" class="floor_add dialog">
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>楼号</template>
+            </el-input>
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>楼层</template>
+            </el-input>
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>每层房数</template>
+            </el-input>
+            <el-button type="primary" class="btn_right">确认新增</el-button>
+        </el-dialog>
+
+        <el-dialog v-model="showFloorUpdate" title="修改宿舍楼" width="30%" :before-close="handleClose" class="floor_update dialog">
+
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>楼号</template>
+            </el-input>
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>楼层</template>
+            </el-input>
+            <el-input v-model="input1" placeholder="Please input">
+                <template #prepend>每层房数</template>
+            </el-input>
+            <el-button type="primary">删除</el-button>
+            <el-button type="primary">修改</el-button>
+
+
+        </el-dialog>
+
+
+        <el-dialog v-model="showAllotRoom" title="分配宿舍" width="30%" :before-close="handleClose" class="allot_room dialog">
+            <!-- 分配宿舍 -->
+
+
+        </el-dialog>
+
+        <div class="building_box">
+            <ul>
+                <li v-for="count in 10">
+                    <div class="left">{{ count }}</div>
+                    <div class="right">
+                        <span :onClick="() => { showFloorUpdate = true }">楼层修改</span>
+                        <span :onClick="() => { showAllotRoom = true }">分配宿舍</span>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+    </main>
 </template>
-<script setup></script>
-<style scoped>
-main{
-    padding: 20px 60px 0px;
+<script setup>
+import { reactive, ref } from 'vue';
+const showFloorUpdate = ref(false);
+const showAllotRoom = ref(false);
+const showFloorAdd = ref(false);
 
-}
-.new_building_btn{
-    background-color: var(--color-blue-dark);
-    text-align: center;
-    font-size: var(--text-2xl);
-    color: #fff;
-    border-radius: 20px;
-    transition: all .2s;
-}
-.new_building_btn:hover{
-    background-color: var(--color-blue-light);
 
-}
+</script>
+<style scoped></style>
 
-</style>
+<style scoped src="../../assets/css/views/admin/buildingManagement.css"></style>
