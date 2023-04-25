b6d5afc8c2205c18e95e9f0815d6d9811368e385
Jet <1298874688@qq.com>
2023-04-25 12:50:26 +0800

    update 配置

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
-const {defineConfig} = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service");
 const AutoImport = require("unplugin-auto-import/webpack");
 const Components = require("unplugin-vue-components/webpack");
-const {VantResolver} = require('unplugin-vue-components/resolvers');
-const {ElementPlusResolver} = require("unplugin-vue-components/resolvers");
+const { VantResolver } = require('unplugin-vue-components/resolvers');
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 
 module.exports = defineConfig({
+    devServer: {
+        proxy: {
+            '/api': {
+                //代理api
+                target: 'http://localhost:9090/', // 代理接口(注意只要域名就够了)
+                changeOrigin: true, //是否跨域
+                ws: true, // proxy websockets
+                pathRewrite: {
+                    //重写路径
+                    '^/api': '' //代理路径
+                }
+            }
+        }
+    },
     //基本路径
     //默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
     publicPath: './',
