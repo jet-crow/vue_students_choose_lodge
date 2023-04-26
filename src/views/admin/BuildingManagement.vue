520e95d07d828300a9ed36bb8d812afd4b1fe50d
Drunkcat7 <Miaopasc@gmail.com>
2023-04-26 09:17:08 +0800

    update BuildingManagement

--- src/views/admin/BuildingManagement.vue
+++ src/views/admin/BuildingManagement.vue
@@ -1,6 +1,6 @@
 <!-- 宿楼管理 -->
 <template>
-    <el-page-header @back="goBack">
+    <el-page-header @back="$router.back()">
         <template #content>
             <span class="text-large font-600 mr-3"> 宿楼管理 </span>
         </template>
@@ -10,8 +10,8 @@
             新建 宿舍楼
         </section>
 
-        <el-dialog v-model="showFloor" title="修改宿舍楼" width="30%" :before-close="handleClose" class="floor_update dialog">
-            <el-input v-model="inputData.buildingName" placeholder="请输入宿舍楼号" v-show="isAdd === true">
+        <el-dialog v-model="showFloor" title="修改宿舍楼" width="30%" class="floor_update dialog">
+            <el-input v-model="inputData.buildingName" placeholder="请输入宿舍楼号" v-show="true">
                 <template #prepend>宿舍楼号</template>
             </el-input>
             <el-input v-model="inputData.buildingFloor" placeholder="请输入宿舍楼层" v-show="isAdd === true">
@@ -26,21 +26,19 @@
             </el-radio-group>
             <template #footer>
                 <el-button type="primary" class="btn_right" v-show="isAdd" @click="newBuilding">确认新增</el-button>
-                <el-button type="primary" v-show="isAdd === false">删除</el-button>
+                <el-button type="primary" v-show="isAdd === false" @click="deleteBuilding">删除</el-button>
                 <el-button type="primary" v-show="isAdd === false">修改</el-button>
             </template>
         </el-dialog>
 
-
-
         <div class="building_box">
             <ul>
-                <li v-for="count in 10">
-                    <div class="sex">男</div>
-                    <div class="left">{{ count }}</div>
+                <li v-for="(item, index) in buildingData">
+                    <div class="sex">{{ item.sex }}</div>
+                    <div class="left">{{ item.buildingName }}</div>
                     <div class="right">
                         <span @click="jump">分配宿舍</span>
-                        <span @click="() => { showFloor = true; isAdd = false; }">楼层修改</span>
+                        <span @click="updateDialog(item)">楼层修改</span>
                     </div>
                 </li>
 
@@ -49,20 +47,43 @@
     </main>
 </template>
 <script setup>
-import { showSuccessToast, showFailToast } from 'vant';
-import { reactive, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref, getCurrentInstance } from 'vue';
+import router from "@/router";
+const { proxy } = getCurrentInstance();
+
 const showFloor = ref(false);
 const isAdd = ref(false);
-const router = useRouter();
-const radio = ref("");
+const radio = ref("男");
 
+const selectedBuilding = ref();
 
 const inputData = ref({
     buildingName: '',
     buildingFloor: '',
     buildingRoomSum: '',
 });
+
+const buildingData = ref([]);
+// 获取网页数据
+const getQueryBuildingAll = () => {
+    proxy.$api.get('/building/queryBuildingAll').then(res => {
+        console.log(res.data);
+        buildingData.value = res.data;
+        // console.log(seriesData.value);
+        buildingData.value.sort(sortBy('buildingName', 0));
+    });
+};
+getQueryBuildingAll();
+// 排序方法 field：字段，direction：1从大到小，0从小到大
+function sortBy(field, direction) {
+    return (x, y) => {
+        if (direction == 0) {
+            return x[field] - y[field];
+        } else {
+            return y[field] - x[field];
+        }
+    };
+}
 
 const newBuilding = () => {
     let data = inputData.value;
@@ -73,13 +94,60 @@ const newBuilding = () => {
         data.buildingRoomSum < 0 ||
         data.buildingFloor < 0 ||
         data.buildingRoomSum == '' ||
-        data.buildingFloor == ''||
-        data.buildingName == ''){
+        data.buildingFloor == '' ||
+        data.buildingName == '') {
+        ElMessage({
+            type: 'error',
+            message: '添加失败，格式有误'
+        });
         return;
     }
-    // 男女也得判断！
+    // 男女也得获取
+    let sex = radio.value;
 
-    console.log('过验证啦');
+    proxy.$api.post("/building/newBuilding", proxy.$qs.stringify({
+        'buildingName': data.buildingName,
+        'buildingFloor': data.buildingFloor,
+        'buildingRoomSum': data.buildingRoomSum,
+        'sex': sex
+    })).then(r => {
+        getQueryBuildingAll();
+        ElMessage({
+            type: 'success',
+            message: '添加成功: ' + data.buildingName + ' 栋,' + '，一共 ' + r.data + ' 间宿舍'
+        });
+    }).catch(_ => {
+        ElMessage({
+            type: 'error',
+            message: '添加失败'
+        });
+    });
+};
+
+const deleteBuilding = () => {
+    proxy.$api.post("/building/deleteBuilding", proxy.$qs.stringify({
+        'buildingId': selectedBuilding.value.buildingId
+    })).then(r => {
+        getQueryBuildingAll();
+        ElMessage({
+            type: 'success',
+            message: '删除成功'
+        });
+    }).catch(_ => {
+        ElMessage({
+            type: 'error',
+            message: '删除失败'
+        });
+    });
+    showFloor.value = false;
+};
+
+const updateDialog = (item) => {
+    showFloor.value = true; 
+    isAdd.value = false; 
+    radio.value = item.sex; 
+    selectedBuilding.value = item;
+    inputData.value.buildingName = item.buildingName;
 };
 
 
@@ -91,8 +159,7 @@ const jump = () => {
             id: '1'
         }
     });
-}
-
+};
 </script>
 <style scoped></style>
 
