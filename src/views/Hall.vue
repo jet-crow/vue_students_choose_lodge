243c4a79a86cd1d223998e1a89df74d7cbc39880
Drunkcat7 <Miaopasc@gmail.com>
2023-04-27 03:38:06 +0800

    update hall

--- src/views/Hall.vue
+++ src/views/Hall.vue
@@ -24,8 +24,8 @@
 
     <main>
         <ul>
-            <li>
-                <div class="room_no">A301</div>
+            <li v-for="(item,index) in roomData">
+                <div class="room_no">{{ item.room.roomName }}</div>
                 <div class="room_info">
                     <div class="lable_tag">
                         <ul>
@@ -36,75 +36,16 @@
                     </div>
                     <div class="room_item">
                         <ul>
-                            <li>
-                                <div class="room_item_box" data_checked="true"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box"></div>
+                            <!-- {{item.userSelectedRoom[i-1]?.bedNumber}} -->
+                            <li v-for="i in 4">
+                                <div class="room_item_box" :data_checked="roomChecked(item,i)"></div>
                             </li>
                         </ul>
                     </div>
                 </div>
             </li>
 
-            <li>
-                <div class="room_no">A301</div>
-                <div class="room_info">
-                    <div class="lable_tag">
-                        <ul>
-                            <li>篮球</li>
-                            <li>唱</li>
-                            <li>rap</li>
-                        </ul>
-                    </div>
-                    <div class="room_item">
-                        <ul>
-                            <li>
-                                <div class="room_item_box" data_checked="true"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box" data_checked="true"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box"></div>
-                            </li>
-                            <li>
-                                <div class="room_item_box"></div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+           
 
             <!-- 这俩不能丢 -->
             <li></li>
@@ -114,13 +55,31 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, getCurrentInstance } from 'vue';
+import router from "@/router";
+const { proxy } = getCurrentInstance();
+
+const roomChecked = (item,i) =>{
+    for(let k = 0; k < 4; k++){
+        if(item.userSelectedRoom[k]?.bedNumber == i){
+            return "true"
+        }
+    }
+}
+
+
+const roomData = ref([]);
+proxy.$api.get('/room/queryMyAllRoom').then(res => {
+        console.log(res.data);
+        roomData.value = res.data;
+        console.log(roomData.value);
+});
+
 // 显示和隐藏搜索框
 const checked_search = ref(false);
 // 顺序显示 和 推荐显示
 const sortChecked = ref(false);
 const sortType = ref("推荐排序");
-
 function sortList() {
     if (sortChecked.value) {
         sortType.value = "推荐排序";
