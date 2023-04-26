3010bb817c0d1e00d6e1233be36b412d11cd4c9a
Jet <1298874688@qq.com>
2023-04-26 18:38:09 +0800

    PickUp END

--- src/views/PickUp.vue
+++ src/views/PickUp.vue
@@ -3,11 +3,12 @@
     <main>
         <p class="title_msg">选择符合自己的标签</p>
         <div class="label_box">
-            <div class="label_items" v-for="item in labelData" :key="item.id">
+            <div class="label_items" v-for="item in labelData" :key="item.tagId">
                 <div class="title"> {{ item.title }}</div>
                 <div class="label_tag">
-                    <span v-for="(tag, index) in item.tag" :key="index">
-                        {{ tag }}
+                    <span v-for="(tag, index) in item.tagContexts" :key="tag.tagId" @click="choose(tag)"
+                        :class="{ 'tag_choose': tag.iSs }">
+                        {{ tag.tagContext }}
                     </span>
                 </div>
             </div>
@@ -18,37 +19,17 @@
             <span>{{ msgCount }}/30</span>
         </div>
         <div class="submit">
-            <div id="Submit">确认无误</div>
+            <div id="Submit" @click="submitTag">确认无误</div>
         </div>
     </main>
 </template>
 <script setup>
+import router from '@/router';
 import Nav from '../components/Nav.vue'
-import { reactive, ref } from 'vue'
-import { p } from '@antfu/utils';
+import { ref, getCurrentInstance } from 'vue'
 
-const labelData = reactive([
-    {
-        id: 0,
-        title: "爱好",
-        tag: ["音乐", "篮球", "音乐", "篮球", "篮球", "音乐", "篮球", "篮球", "音乐", "篮球"]
-    },
-    {
-        id: 1,
-        title: "入睡时间",
-        tag: ["音乐", "篮球", "音乐", "篮球"]
-    },
-    {
-        id: 2,
-        title: "生活词",
-        tag: ["音乐", "篮球", "音乐", "篮球"]
-    },
-    {
-        id: 3,
-        title: "kes",
-        tag: ["音乐", "篮球", "音乐", "篮球"]
-    },
-])
+const { proxy } = getCurrentInstance();
+const labelData = ref([])
 //字数限制
 const personalMsg = ref("")
 const msgCount = ref(0)
@@ -58,7 +39,77 @@ function checkMsgNumbe() {
     personalMsg.value = personalMsg.value.substring(0, 30);
     msgCount.value = (personalMsg.value).length
 }
+//如果沒有修改機會直接跳轉hall頁面
+proxy.$api.get("/userInfo/checkUserTag").then(r => {
+    if (r.data) {
+        router.replace("/hall");
+    }
+})
+//请求
+proxy.$api.get("/tag/tagAll").then(r => {
+    // console.log(r.data);
+    labelData.value = r.data.reduce((acc, cur) => {
+        const index = acc.findIndex(item => item.title === cur.tagTitle);
+        if (index >= 0) {
+            acc[index].tagContexts.push({
+                tagContext: cur.tagContext,
+                tagId: cur.tagId,
+                iSs: false
+            });
+        } else {
+            acc.push({
+                title: cur.tagTitle,
+                tagContexts: [{
+                    tagContext: cur.tagContext,
+                    tagId: cur.tagId,
+                    iSs: false
+                }]
+            });
+        }
+        return acc;
+    }, []);
+    console.log(labelData.value);
+})
+//选择
+const tagChooseData = [];
+const choose = (tag) => {
+    tag.iSs = !tag.iSs;
+    if (tag.iSs == true) {
+        tagChooseData.push(tag.tagId)
+    } else {
+        tagChooseData.splice(tagChooseData.indexOf(tag.tagId), 1)
+    }
+    // console.log(tag);
+    // console.log(tagChooseData);
+}
+//提交
+const submitTag = () => {
+    if (msgCount.value != 0) {
+        //修改个签
+        proxy.$api.put("/userInfo/updateIntroduce", proxy.$qs.stringify({
+            introduce: personalMsg.value
+        }))
+    }
+    proxy.$api.get("/userSelectedTag/isUpdateSelectedTags").then(r => {
+        if (r.data) {//修改,    //如果选择过就调用修改接口
+            proxy.$api.post("/userSelectedTag/updateTag", proxy.$qs.stringify({
+                tagIds: tagChooseData.toString()
+            })).then(r => {
+                proxy.$showSuccessToast("修改成功")
+                router.push("/hall")
+            })
+            return
+        }
+        //新增
+        proxy.$api.post("/userSelectedTag/addTag", proxy.$qs.stringify({
+            tagIds: tagChooseData.toString()
+        })).then(r => {
+            proxy.$showSuccessToast("添加成功")
+            router.push("/hall")
+        })
+    })
+
+}
 </script>
 
-<style scoped src="../assets/css/views/pickUp.css">
-</style>
+<style scoped src="../assets/css/views/pickUp.css"></style>
