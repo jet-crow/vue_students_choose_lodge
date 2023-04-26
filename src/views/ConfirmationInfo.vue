cea86dede2e7b12f016203447f56552f38243ac7
Jet <1298874688@qq.com>
2023-04-26 09:59:01 +0800

    add pickUp END

--- src/views/ConfirmationInfo.vue
+++ src/views/ConfirmationInfo.vue
@@ -1,35 +1,35 @@
 <!-- ConfirmationInfo 确认信息页 -->
 <template>
     <div id="Bg">
-        <Nav :title="'确认信息'"/>
+        <Nav :title="'确认信息'" />
         <main>
             <div class="confirmationInfo_from">
                 <div>
                     <span>专业</span>
-                    <input type="text" readonly="readonly" value="软件工程">
+                    <input type="text" readonly="readonly" :value="userData?.professional">
                 </div>
                 <div>
                     <span>考生号</span>
-                    <input type="text" readonly="readonly" value="33212223">
+                    <input type="text" readonly="readonly" :value="userData?.studentId">
                 </div>
                 <div>
                     <span>姓名</span>
-                    <input type="text" readonly="readonly" value="小明">
+                    <input type="text" readonly="readonly" :value="userData?.name">
                 </div>
                 <div>
                     <span>性别</span>
-                    <input type="text" readonly="readonly" value="男">
+                    <input type="text" readonly="readonly" :value="userData?.sex">
                 </div>
                 <div>
                     <span>电话</span>
-                    <input type="text" readonly="readonly" value="13900000000">
+                    <input type="text" readonly="readonly" :value="userData?.tel">
                 </div>
                 <div class="id_card">
                     <span>身份证</span>
-                    <input type="text" readonly="readonly" value="4210000000000000000">
+                    <input type="text" readonly="readonly" :value="userData?.idCard">
                 </div>
                 <div class="submit">
-                    <div id="Submit">确认无误</div>
+                    <div id="Submit" @click="determine">确认无误</div>
                     <span style="color:#c81623">注: 如信息有误，请联系教务处。</span>
                 </div>
             </div>
@@ -37,9 +37,29 @@
     </div>
 </template>
 <script setup>
+import router from '@/router';
 import Nav from '../components/Nav.vue'
+import { getCurrentInstance, ref } from "vue"
+//是否已经确认信息，若已经确认信息就跳转pickUp
+const { proxy } = getCurrentInstance();
+const userData = ref()
 
+proxy.$api.get("/userInfo/myUserInfo").then(r => {
+    if (r.data.isConfirmUserInfo == 1) {
+        router.replace("/pickUp")
+    }
+    userData.value = r.data;
+    console.log(r.data);
+})
+//确定
+const determine = () => {
+    proxy.$api.get("/userInfo/confirmUserInfo?userInfoId=" + userData.value.userInfoId).then(r => {
+        if (r.data) {
+            proxy.$showSuccessToast("确认成功")
+            router.push("/pickUp");
+        }
+    })
+}
 </script>
 
-<style scoped src="../assets/css/views/confirmationInfo.css">
-</style>
+<style scoped src="../assets/css/views/confirmationInfo.css"></style>
