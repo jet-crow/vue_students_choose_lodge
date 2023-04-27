9bd1b1f3e69eae47152ab8caaf2988288a93eb90
Drunkcat7 <Miaopasc@gmail.com>
2023-04-27 14:25:22 +0800

    update FINISH

--- src/views/HallInfo.vue
+++ src/views/HallInfo.vue
@@ -15,7 +15,8 @@
         <div class="user_info">
             <ul>
                 <li :class="roomData.find(r => r.bedNumber == i) != null ? 'user_info_true' : 'user_info_false'"
-                    v-for="(i, index) in 4" @click="roomData.find(r => r.bedNumber == i) == null ? joinRoom(i) : null">
+                    v-for="(i, index) in 4"
+                    @click="roomData.find(r => r.bedNumber == i) == null ? joinRoomDialog(i) : null">
                     <!-- {{ roomData.find(r => r.bedNumber == i)  }} -->
                     <template v-if="roomData.find(r => r.bedNumber == i) != null">
                         <!-- 上部 名字 标签 -->
@@ -51,15 +52,15 @@
     <van-popup v-model:show="showRoom" round class="room_popup">
         <h2>协议</h2>
         <p class="room_msg">
-            <span>房间:A301</span>
-            <span>床位:1</span>
+            <span>房间:{{ roomName }}</span>
+            <span>床位:{{ joinData.bedNumber }}</span>
         </p>
         <p class="popup_msg">
             是否<strong>确认选择</strong>，同意后<strong>无法更改</strong>，如有疑问请联系辅导员老师。
         </p>
         <div class="popup_btn">
             <van-button plain hairline type="danger" size="small" @click="showRoom = false">不同意</van-button>
-            <van-button color="#1C4C8C" size="small" plain>同意</van-button>
+            <van-button color="#1C4C8C" size="small" plain @click="joinRoomSubmit">同意</van-button>
         </div>
     </van-popup>
 </template>
@@ -71,6 +72,16 @@ const roomId = router.currentRoute.value.query.roomId;
 const roomName = router.currentRoute.value.query.roomName;
 const buildingId = router.currentRoute.value.query.buildingId;
 const { proxy } = getCurrentInstance();
+//弹窗
+const showRoom = ref(false);
+//是否展开
+const opne = ref(false);
+// 提交的房间
+const joinData = ref({
+    buildingId: '',
+    roomId: '',
+    bedNumber: ''
+});
 //获取共同标签
 const commonTag = ref([]);
 proxy.$api.get("/room/userCommonTag?roomId=" + roomId).then(r => {
@@ -89,17 +100,48 @@ const tags = reactive([{
     tag: "篮球"
 }]);
 
-//弹窗
-const showRoom = ref(false);
-//是否展开
-const opne = ref(false);
 
-//加入房间
-function joinRoom(bedNumber) {
-    console.log(roomId);
-    console.log(bedNumber);
-    console.log(buildingId);
-}
+//加入房间弹窗
+const joinRoomDialog = (bedNumber) => {
+    showRoom.value = true;
+    joinData.value.buildingId = buildingId;
+    joinData.value.roomId = roomId;
+    joinData.value.bedNumber = bedNumber;
+    // console.log(joinData.value);
+};
+
+// 加入房间
+const joinRoomSubmit = () => {
+    // console.log(joinData.value);
+    proxy.$api.post("/userSelectedRoom/insertUserRoom", proxy.$qs.stringify({
+        buildingId: joinData.value.buildingId,
+        roomId: joinData.value.roomId,
+        bedNumber: joinData.value.bedNumber
+    })).then(r => {
+        if (r.data) {
+            ElMessage({
+                message: '加入成功',
+                type: 'success',
+            });
+            location.reload();
+        } else {
+            ElMessage({
+                message: '您已有宿舍了哦',
+                type: 'error',
+            });
+            showRoom.value = false;
+
+        }
+
+    }).catch(e => {
+        ElMessage({
+            message: '加入失败',
+            type: 'error',
+        });
+        showRoom.value = false;
+
+    });
+};
 </script>
 
 <style scoped src="../assets/css/views/hallInfo.css"></style>
