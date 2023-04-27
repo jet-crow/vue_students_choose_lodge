ee366c86cd1b67d97e0462b33d6d6ac8eae44976
Jet <1298874688@qq.com>
2023-04-27 13:54:54 +0800

    房间管理页END

--- src/views/admin/RoomManagement.vue
+++ src/views/admin/RoomManagement.vue
@@ -8,10 +8,26 @@
     <main>
         <el-table :data="filterTableData" border>
             <el-table-column label="房间号" prop="roomId" align="center" />
-            <el-table-column label="一号床铺" prop="bed1" align="center" width="200" />
-            <el-table-column label="二号床铺" prop="bed2" align="center" width="200" />
-            <el-table-column label="三号床铺" prop="bed3" align="center" width="200" />
-            <el-table-column label="四号床铺" prop="bed4" align="center" width="200" />
+            <el-table-column label="一号床铺" align="center" width="200">
+                <template #default="scope">
+                    {{ scope.row.bedNumber1?.studentId }} {{ scope.row.bedNumber1?.name }}
+                </template>
+            </el-table-column>
+            <el-table-column label="二号床铺" align="center" width="200">
+                <template #default="scope">
+                    {{ scope.row.bedNumber2?.studentId }} {{ scope.row.bedNumber2?.name }}
+                </template>
+            </el-table-column>
+            <el-table-column label="三号床铺" align="center" width="200">
+                <template #default="scope">
+                    {{ scope.row.bedNumber3?.studentId }} {{ scope.row.bedNumber3?.name }}
+                </template>
+            </el-table-column>
+            <el-table-column label="四号床铺" align="center" width="200">
+                <template #default="scope">
+                    {{ scope.row.bedNumber4?.studentId }} {{ scope.row.bedNumber4?.name }}
+                </template>
+            </el-table-column>
 
             <el-table-column align="right" width="200">
                 <template #header>
@@ -30,8 +46,8 @@
         </el-table>
     </main>
     <!-- 学生信息弹窗 -->
-    <el-dialog v-model="showStudent" :title="roomInfo.title" width="1190"  @close="closeRoomInfo">
-        <el-table :data="roomInfo.beds" border>
+    <el-dialog v-model="showStudent" :title="roomInfo.title" width="1190" @close="closeRoomInfo">
+        <el-table :data="roomInfo.bedNumbers" border>
             <el-table-column label="床位" prop="id" align="center" width="80" />
             <el-table-column label="考生号" prop="studentId" align="center" width="120" />
             <el-table-column label="姓名" prop="name" align="center" width="120" />
@@ -49,56 +65,45 @@
 </template>
 
 <script setup>
-import { computed, ref, reactive } from 'vue'
+import { computed, ref, reactive, getCurrentInstance } from 'vue'
 
-const tableData = reactive([
-    {
-        roomId: 1008611,
-        bed1: "陈冠希 sadqwe",
-        bed2: "陈冠希 22215150317",
-        bed3: "陈冠希 22215150317",
-        bed4: "陈冠希 22215150317",
-    },
-    {
-        roomId: 1008611,
-        bed1: "陈冠希 22215150317",
-        bed2: "陈冠希 22215150317",
-        bed3: "陈冠希 22215150317",
-        bed4: "陈冠希 22215150317",
-    },
-    {
-        roomId: 1008611,
-        bed1: "2131232",
-        bed2: "",
-        bed3: "",
-        bed4: "",
-    },
-    {
-        roomId: 1008612,
-        bed1: "陈冠希 22215150317",
-        bed2: "陈冠希 22215150317",
-        bed3: "陈冠希 22215150317",
-        bed4: "陈冠希 22215150317",
-    }
-]);
+const { proxy } = getCurrentInstance();
+const tableData = ref([{
+    roomId: 1008611,
+    bedNumber1: "陈冠希 sadqwe",
+    bedNumber2: "陈冠希 22215150317",
+    bedNumber3: "陈冠希 22215150317",
+    bedNumber4: "陈冠希 22215150317",
+}]);
+//获取房间管理信息
+proxy.$api.get("/userSelectedRoom/roomManagementInfo").then(r => {
+    tableData.value = r.data;
+    console.log(r.data);
+})
+
 const goBack = () => {
     console.log('go back')
 }
+//搜索
 const search = ref('')
 const filterTableData = computed(() =>
-    tableData.filter((data) => !search.value ||
+    tableData.value.filter((data) => !search.value ||
         (data.roomId + "").includes(search.value) ||
-        (data.bed1).includes(search.value) ||
-        (data.bed2).includes(search.value) ||
-        (data.bed3).includes(search.value) ||
-        (data.bed4).includes(search.value))
-)
+        (data.bedNumber1?.studentId + "").includes(search.value) ||
+        (data.bedNumber1?.name + "").includes(search.value) ||
+        (data.bedNumber2?.studentId + "").includes(search.value) ||
+        (data.bedNumber2?.name + "").includes(search.value) ||
+        (data.bedNumber3?.studentId + "").includes(search.value) ||
+        (data.bedNumber3?.name + "").includes(search.value) ||
+        (data.bedNumber4?.studentId + "").includes(search.value) ||
+        (data.bedNumber4?.name + "").includes(search.value)))
+
 //取消学生的选择
 const showStudent = ref(false);
 //请求这个房间的信息==>用房间的唯一id请求
 const roomInfo = ref({
     title: "26栋A103",
-    beds: [
+    bedNumbers: [
         {
             id: 1,
             studentId: 123456789,
@@ -136,32 +141,53 @@ const roomInfo = ref({
 })
 let selectRoomIndex;
 const handleEdit = (index, row) => {
-    //在这里请求
     selectRoomIndex = index;
     showStudent.value = true;
-    // console.log(index, row)
+    roomInfo.value = {
+        title: row.title,
+        bedNumbers: []
+    }
+    //弹窗的数据
+    for (let index = 1; index <= 4; index++) {
+        let roomUserInfo = row["bedNumber" + index]
+        if (roomUserInfo == undefined) {
+            continue;
+        }
+        roomInfo.value.bedNumbers.push({
+            id: index,
+            uid: roomUserInfo.uid,
+            studentId: roomUserInfo.studentId,
+            name: roomUserInfo.name,
+            professional: roomUserInfo.professional,
+            sex: roomUserInfo.sex,
+            tel: roomUserInfo.tel,
+            idCard: roomUserInfo.idCard
+        })
+    }
 }
 
 const cancel = (index, data) => {
     //删除弹窗的单行
-    tableData[selectRoomIndex]['bed' + data.id] = "";
+    tableData.value[selectRoomIndex]['bedNumber' + data.id] = undefined;
     //在删除的时候 将外面表格的床铺置为空
-    roomInfo.value.beds.splice(index, 1);
+    let uid = roomInfo.value.bedNumbers[index].uid;
+    roomInfo.value.bedNumbers.splice(index, 1);
     ElMessage({
-        type:'success',
-        message:"取消选择成功"
+        type: 'success',
+        message: "取消选择成功"
     })
+    //用户取消选择
+    proxy.$api.get("/userSelectedRoom/deselectUserRoom?uid=" + uid);
 }
 //关闭房间信息弹窗
 const closeRoomInfo = () => {
     //关闭弹窗检测是否所有床铺都为空 若为空则删除这行数据 
-    // console.log(tableData[selectRoomIndex]);
     let room = null;
     for (let i = 1; i <= 4; i++) {
-        room = tableData[selectRoomIndex]['bed' + i]
-        if (room != "") return;
+        room = tableData.value[selectRoomIndex]['bedNumber' + i]
+        if (room != undefined) return;
     }
-    tableData.splice(selectRoomIndex,1);
+    tableData.value.splice(selectRoomIndex, 1);
 }
 
 </script>
